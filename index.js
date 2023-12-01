c5f6439ef706d547f1fd165b67329d0ccd91ea17
forhad446 <forhadkkhan@gmail.com>
2023-12-01 21:33:04 +0600

    database connect and some secure by env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -8,7 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors())
 
-const uri = "mongodb+srv://forhad445:hCYCLsss09eq1Cud@cluster0.ajryzz7.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ajryzz7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +29,7 @@ async function run() {
         const usersCollection = client.db("usersDB").collection("users");
         const postsCollection = client.db("usersDB").collection("posts");
         const trainersCollection = client.db("trainersDB").collection("trainers");
+        const classCollection = client.db("trainersDB").collection("class");
 
         // new users who created account on my website
         app.post('/users', async (req, res) => {
@@ -46,6 +49,12 @@ async function run() {
             const result = await trainersCollection.insertOne(data);
             res.send(result)
         })
+        // for adding new class
+        app.post('/class', async (req, res) => {
+            const data = req.body;
+            const result = await classCollection.insertOne(data);
+            res.send(result)
+        })
         // get all post
         app.get('/posts', async (req, res) => {
             const result = await postsCollection.find().toArray();
@@ -54,6 +63,11 @@ async function run() {
         // get all trainer
         app.get('/trainers', async (req, res) => {
             const result = await trainersCollection.find().toArray();
+            res.send(result)
+        })
+        // get all class  
+        app.get('/class', async (req, res) => {
+            const result = await classCollection.find().toArray();
             res.send(result)
         })
         // put user by id
@@ -72,11 +86,11 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.send(result) 
         })
-        // put user by email
-        app.put('/users/:email', async (req, res) => {
+        // put user by email 
+        app.put('/dbUsers/:email', async (req, res) => {
             const email = req.params.email;
 
-            const options = { upsert: true };
+            const options = { upsert: true }; 
             const filter = { email: email };
             const updateDoc = {
                 $set: {
